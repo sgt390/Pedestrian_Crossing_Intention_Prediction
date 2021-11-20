de845285cf3fbec47814bc0b59b02b872361e70e
sgt390 <stefano.sgt39@gmail.com>
2021-11-20 10:53:20 +0100

    custom cnn

--- action_predict_pie.py
+++ action_predict_pie.py
@@ -1635,6 +1635,7 @@ class MASK_PCPA_4_2D_CNN(ActionPredict):
         x = self._rnn(name='enc0_' + data_types[0], r_sequence=return_sequence)(network_inputs[0])
         encoder_outputs.append(x)
         x = self._3dconv()(network_inputs[1])
+        x = Flatten()(x)
         x = self._rnn(name='enc1_' + data_types[1], r_sequence=return_sequence)(x)  # (tf.squeeze(x, [1, 2]))
         encoder_outputs.append(x)
         x = self._rnn(name='enc2_' + data_types[2], r_sequence=return_sequence)(network_inputs[2])
