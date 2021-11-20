be5ee6ca4dfc5e1fc90d25620a858eb239d46259
sgt390 <stefano.sgt39@gmail.com>
2021-11-20 17:07:01 +0100

    custom cnn

--- train_test_pie.py
+++ train_test_pie.py
@@ -89,7 +89,7 @@ def run(config_file=None):
         configs['train_opts']['batch_size'] = model_configs['exp_opts']['batch_size'][dataset_idx]
         configs['train_opts']['lr'] = model_configs['exp_opts']['lr'][dataset_idx]
         configs['train_opts']['epochs'] = model_configs['exp_opts']['epochs'][dataset_idx]
-        configs['train_opts']['learning_scheduler'] = {'learning_scheduler':{'early_stop':{'min_delta': 0.015, 'patience':5, 'monitor':'binary_crossentropy', 'restore_best_weights': True}}}
+        configs['train_opts']['learning_scheduler'] = {'learning_scheduler':{'early_stop':{'min_delta': 0.015, 'patience':5, 'restore_best_weights': True}}}
         # configs['train_opts']['learning_scheduler'] = set(model_configs['exp_opts']['learning_scheduler'][dataset_idx]) if 'learning_scheduler' in model_configs['exp_opts'] else None
 
         model_name = configs['model_opts']['model']
