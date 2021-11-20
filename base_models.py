c49a34a6cac21b2c643b0478afc90e7ad48e505f
sgt390 <stefano.sgt39@gmail.com>
2021-11-20 14:47:39 +0100

    custom cnn

--- base_models.py
+++ base_models.py
@@ -494,6 +494,9 @@ def SIMPLE_CNN(freeze_conv_layers=False, weights=None,
     # 2nd layer group
     model = Conv3D(16, 3, activation='relu', padding='same', name='conv2')(model)
     model = MaxPooling3D(pool_size=(4, 4, 4), strides=(2, 2, 2), padding='valid', name='pool2')(model)
+    # 2nd layer group
+    model = Conv3D(16, 3, activation='relu', padding='same', name='conv2')(model)
+    model = MaxPooling3D(pool_size=(3, 55, 55), strides=(2, 2, 2), padding='valid', name='pool2')(model)
 
     net_model = Model(input_data, model)
 
