b28c6e3b45f3653c608af00072ed8e34da19127f
sgt390 <stefano.sgt39@gmail.com>
2021-12-03 12:02:44 +0100

    split

--- action_predict_pie.py
+++ action_predict_pie.py
@@ -1016,9 +1016,9 @@ class ActionPredict:
         Returns:
             A list of call backs or None if learning_scheduler is false
         """
-        wandb_callback = WandbCallback(log_evaluation=True)
-        callbacks = [wandb_callback]
-        # callbacks = []
+        # wandb_callback = WandbCallback(log_evaluation=True)
+        # callbacks = [wandb_callback]
+        callbacks = []
 
         # Set up learning schedulers
         if learning_scheduler:
