c1f02add6ec885a42c207dbacd76a1912d88deb7
sgt390 <stefano.sgt39@gmail.com>
2022-01-05 18:32:13 +0100

    split

--- action_predict_attention.py
+++ action_predict_attention.py
@@ -1054,9 +1054,9 @@ class ActionPredict:
         Returns:
             A list of call backs or None if learning_scheduler is false
         """
-        # wandb_callback = WandbCallback(log_evaluation=True)
-        # callbacks = [wandb_callback]
-        callbacks = [] # todo swap
+        wandb_callback = WandbCallback(log_evaluation=True)
+        callbacks = [wandb_callback]
+        # callbacks = [] # todo swap
 
 
 
