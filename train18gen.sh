d6587f9a76f321f1b9cec370f62ac830dcd35713
sgt390 <stefano.sgt39@gmail.com>
2022-01-18 23:38:36 +0100

    transformer 4 layers, 8 heads, 0.5 dropout

---
+++ train18gen.sh
@@ -0,0 +1,3 @@
+python train_test_attention.py -c config_files/norange15/MASK_PCPA_2d_15.yaml
+python train_test_attention.py -c config_files/norange30/MASK_PCPA_2d_30.yaml
+python train_test_attention.py -c config_files/norange60/MASK_PCPA_2d_60.yaml
