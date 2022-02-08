f4b084ab29c003f105592975ad3386336734e37f
sgt390 <stefano.sgt39@gmail.com>
2022-02-08 14:34:57 +0100

    batch normalization on hybrid (original)

--- base_models.py
+++ base_models.py
@@ -569,30 +569,31 @@ class Time2Vec(keras.layers.Layer):
 # def ModelTrunk(name='ModelTrunk', time2vec_dim=1, num_heads=2, head_size=128, ff_dim=None, num_layers=1,
 #                  dropout=0, representation_size=None, input_data=Input(shape=(16, 512))):
 def ModelTrunk(name='ModelTrunk', time2vec_dim=1, num_heads=4, head_size=128, ff_dim=None, num_layers=2,
-               dropout=0.4, representation_size=None, input_data=Input(shape=(16, 512))):
-        time2vec = Time2Vec(kernel_size=time2vec_dim)
-        timedist = keras.layers.TimeDistributed(time2vec)
-        if ff_dim is None:
-            ff_dim = head_size
-        attention_layers = [AttentionBlock(num_heads=num_heads, head_size=head_size, ff_dim=ff_dim, dropout=dropout) for _ in range(num_layers)]
+               dropout=0.4, representation_size=None, input_shape=(16, 512)):
+    input_data = Input(input_shape)
+    time2vec = Time2Vec(kernel_size=time2vec_dim)
+    timedist = keras.layers.TimeDistributed(time2vec)
+    if ff_dim is None:
+        ff_dim = head_size
+    attention_layers = [AttentionBlock(num_heads=num_heads, head_size=head_size, ff_dim=ff_dim, input_shape=(input_shape[0], input_shape[1]*3), dropout=dropout) for _ in range(num_layers)]
 
-        dense0 = tf.keras.layers.Dense(representation_size*12, name=name+"resizing0", activation="relu")
-        dense1 = tf.keras.layers.Dense(representation_size*6, name=name+"resizing1", activation="relu")
-        dense2 = tf.keras.layers.Dense(representation_size, name=name+"resizing2", activation="relu")
-        keras_dropout0 = Dropout(dropout)
-        keras_dropout1 = Dropout(dropout)
-        keras_dropout2 = Dropout(dropout/2)
+    dense0 = tf.keras.layers.Dense(representation_size*12, name=name+"resizing0", activation="relu")
+    dense1 = tf.keras.layers.Dense(representation_size*6, name=name+"resizing1", activation="relu")
+    dense2 = tf.keras.layers.Dense(representation_size, name=name+"resizing2", activation="relu")
+    keras_dropout0 = Dropout(dropout)
+    keras_dropout1 = Dropout(dropout)
+    keras_dropout2 = Dropout(dropout/2)
 
-        time_embedding = timedist(input_data)
-        x = K.concatenate([input_data, time_embedding], -1)
-        for attention_layer in attention_layers:
-            x = attention_layer(x)
-        x = K.reshape(x, (-1, x.shape[1] * x.shape[2]))  # flat vector of features out
-        x = keras_dropout0(x)
-        x = dense0(x)
-        x = keras_dropout1(x)
-        x = dense1(x)
-        x = keras_dropout2(x)
-        x = dense2(x)
-        x = K.expand_dims(x, 1)
-        return Model(input_data, x)
+    time_embedding = timedist(input_data)
+    x = K.concatenate([input_data, time_embedding], -1)
+    for attention_layer in attention_layers:
+        x = attention_layer(x)
+    x = K.reshape(x, (-1, x.shape[1] * x.shape[2]))  # flat vector of features out
+    x = keras_dropout0(x)
+    x = dense0(x)
+    x = keras_dropout1(x)
+    x = dense1(x)
+    x = keras_dropout2(x)
+    x = dense2(x)
+    x = K.expand_dims(x, 1)
+    return Model(input_data, x)
