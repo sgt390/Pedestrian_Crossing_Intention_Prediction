e3470e816eb571e928fadc8903655c06db5205eb
sgt390 <stefano.sgt39@gmail.com>
2022-03-08 12:51:05 +0100

    final_2: tst with early concatenation

--- base_models.py
+++ base_models.py
@@ -581,39 +581,39 @@ def ModelTrunk(name='ModelTrunk', time2vec_dim=1, num_heads=4, head_size=128, ff
         ff_dim = head_size
     attention_layers = [AttentionBlock(num_heads=num_heads, head_size=head_size, ff_dim=ff_dim, input_shape=(input_shape[0], input_shape[1]*3), dropout=dropout) for _ in range(num_layers)]
 
-    dense0 = tf.keras.layers.Dense(representation_size*12, name=name+"resizing0", activation="relu",
-                                   kernel_initializer='random_normal',
-                                   bias_initializer='zeros'
-                                   )
-    dense1 = tf.keras.layers.Dense(representation_size*6, name=name+"resizing1", activation="relu",
-                                   kernel_initializer='random_normal',
-                                   bias_initializer='zeros'
-                                   )
+    # dense0 = tf.keras.layers.Dense(representation_size*12, name=name+"resizing0", activation="relu",
+    #                                kernel_initializer='random_normal',
+    #                                bias_initializer='zeros'
+    #                                )
+    # dense1 = tf.keras.layers.Dense(representation_size*6, name=name+"resizing1", activation="relu",
+    #                                kernel_initializer='random_normal',
+    #                                bias_initializer='zeros'
+    #                                )
     dense2 = tf.keras.layers.Dense(representation_size, name=name+"resizing2", activation="relu",
                                    kernel_initializer='random_normal',
                                    bias_initializer='zeros'
                                    )
-    keras_dropout0 = Dropout(dropout)
-    keras_dropout1 = Dropout(dropout)
-    keras_dropout2 = Dropout(dropout/2)
+    # keras_dropout0 = Dropout(dropout)
+    # keras_dropout1 = Dropout(dropout)
+    keras_dropout2 = Dropout(dropout)
 
     time_embedding = timedist(input_data)
     x = K.concatenate([input_data, time_embedding], -1)
     for attention_layer in attention_layers:
         x = attention_layer(x)
     x = K.reshape(x, (-1, x.shape[1] * x.shape[2]))  # flat vector of features out
-    if include_dense_0:
-        x = keras_dropout0(x)
-        x = dense0(x)
-    x = keras_dropout1(x)
-    x = dense1(x)
+    # if include_dense_0:
+    #     x = keras_dropout0(x)
+    #     x = dense0(x)
+    # x = keras_dropout1(x)
+    # x = dense1(x)
     x = keras_dropout2(x)
     x = dense2(x)
     x = K.expand_dims(x, 1)
     model = Model(input_data, x)
     model.layers[7].trainable = False  # freeze reshape
-    model.layers[9].trainable = False
-    model.layers[11].trainable = False
+    # model.layers[9].trainable = False
+    # model.layers[11].trainable = False
     return model
 
 
